78fe8f0c271181ae326cea0441dbb506c0aef076
Gabriela S Santos <gabriela.santos@instruct.com.br>
2022-09-21 10:28:36 -0300

    resolucao hamming

--- exercicios/dia3/hamming/hamming.py
+++ exercicios/dia3/hamming/hamming.py
@@ -1,2 +1,11 @@
 def distance(strand_a, strand_b):
-    pass
+    if (len(strand_a) != len(strand_b)):
+        raise ValueError("Strands must be of equal length.")
+    else:
+        hamming = 0
+        for (letter_a, letter_b) in zip(strand_a,strand_b):
+            if (letter_a != letter_b):
+                hamming +=1
+
+    return hamming
+
